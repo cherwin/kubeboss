080222fa22d6321ff199e012d6cf18741922649e
Cherwin Nooitmeer <cherwin@unmanaged.io>
2023-12-20 15:36:43 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Kubernetes like a BOSS
---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "region" {
+  default = "ams3"
+}
+
+variable "k8s_clustername" {
+  default = "clusterwim"
+}
+
+variable "k8s_version" {
+  default = "1.19.3-do.3"
+}
+
+variable "k8s_poolname" {
+  default = "worker-pool"
+}
+
+variable "k8s_count" {
+  default = "3"
+}
